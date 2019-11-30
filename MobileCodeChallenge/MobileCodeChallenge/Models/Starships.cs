e5b50b7e3d00cd28b1477de2f2268bf19a8d909e
Chris Piccaro <chrispiccaro18@gmail.com>
2019-11-30 12:08:12 -0800

    added Starships model

---
+++ MobileCodeChallenge/MobileCodeChallenge/Models/Starships.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MobileCodeChallenge.Models
+{
+    class Starships
+    {
+        public List<Starship> starships { get; set; }
+
+        public List<Starship> SortStarshipsAlphabetically()
+        {
+            this.starships.Sort(delegate (Starship x, Starship y)
+            {
+                if (x.Name == null && y.Name == null) return 0;
+                else if (x.Name == null) return -1;
+                else if (y.Name == null) return 1;
+                else return x.Name.CompareTo(y.Name);
+            });
+
+            return starships;
+        }
+    }
+}
