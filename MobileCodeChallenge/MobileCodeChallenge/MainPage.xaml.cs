aeab471337d4729af169e2a62521597fd66cb5dd
Chris Piccaro <chrispiccaro18@gmail.com>
2019-12-01 10:33:29 -0800

    added NotifyTaskCompletion for async but not working

--- MobileCodeChallenge/MobileCodeChallenge/MainPage.xaml.cs
+++ MobileCodeChallenge/MobileCodeChallenge/MainPage.xaml.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using MobileCodeChallenge.Models;
+using MobileCodeChallenge.Services;
+using MobileCodeChallenge.ViewModels;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -12,9 +14,17 @@ namespace MobileCodeChallenge
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class MainPage : ContentPage
     {
+        public NotifyTaskCompletion<List<Starship>> Starships { get; private set; }
         public MainPage()
         {
             InitializeComponent();
+            /*var starshipManager = new StarshipManager( new RestService());
+            Starships = new NotifyTaskCompletion<List<Starship>>(
+                starshipManager.GetStarshipsAsync());
+            if (Starships.IsSuccessfullyCompleted)
+            {
+                listView.ItemsSource = Starships.Result;
+            }*/
         }
     }
 }
---
+++ MobileCodeChallenge/MobileCodeChallenge/ViewModels/StarshipsViewModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using System.Threading.Tasks;
+using MobileCodeChallenge.Models;
+using MobileCodeChallenge.Services;
+using Xamarin.Forms;
+
+namespace MobileCodeChallenge.ViewModels
+{
+    public class StarshipsViewModel
+    {
+        public static StarshipManager starshipManager { get; private set; }
+        List<Starship> allStarships
+        {
+            get { return allStarships;  }
+            set
+            {
+                allStarships = value;
+                LoadStarshipsCommand.ChangeCanExecute();
+            }
+        }
+        public Command LoadStarshipsCommand { get; }
+        public StarshipsViewModel()
+        {
+            LoadStarshipsCommand = new Command(async () => await LoadStarships());
+        }
+
+        async Task LoadStarships()
+        {
+            starshipManager = new StarshipManager(new RestService());
+            allStarships = await starshipManager.GetStarshipsAsync();
+            allStarships.Sort(delegate (Starship x, Starship y)
+            {
+                if (x.Name == null && y.Name == null) return 0;
+                else if (x.Name == null) return -1;
+                else if (y.Name == null) return 1;
+                else return x.Name.CompareTo(y.Name);
+            });
+        }
+    }
+
+
+}
