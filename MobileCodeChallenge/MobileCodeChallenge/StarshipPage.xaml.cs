4f9f5f0c350f7f24398f867cda15d415071819cb
Chris Piccaro <chrispiccaro18@gmail.com>
2019-12-01 10:58:53 -0800

    detail page populating

--- MobileCodeChallenge/MobileCodeChallenge/StarshipPage.xaml.cs
+++ MobileCodeChallenge/MobileCodeChallenge/StarshipPage.xaml.cs
@@ -13,26 +13,10 @@ namespace MobileCodeChallenge
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class StarshipPage : ContentPage
     {
-        public Starship thisStarship { get; set; }
         public StarshipPage(Starship starship)
         {
-            thisStarship = starship;
             InitializeComponent();
-            Console.WriteLine(starship.Name);
-        }
-
-        protected async override void OnAppearing()
-        {
-            base.OnAppearing();
-            StarshipImageService imageService = new StarshipImageService();
-            string starshipImageUrl = await imageService.GetImageUrl(thisStarship.Name);
-            thisStarship.ImageUrl = starshipImageUrl;
-            starshipImage.Source = new UriImageSource()
-            {
-                Uri = new Uri(starshipImageUrl),
-                CachingEnabled = false
-            };
-            BindingContext = thisStarship;
+            BindingContext = starship;
         }
     }
 }
