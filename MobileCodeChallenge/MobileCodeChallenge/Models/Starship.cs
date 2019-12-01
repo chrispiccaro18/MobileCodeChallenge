759213c68fe0c621bacc6df3911c66d547c67976
Chris Piccaro <chrispiccaro18@gmail.com>
2019-11-30 19:51:02 -0800

    added models

---
+++ MobileCodeChallenge/MobileCodeChallenge/Models/Starship.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace MobileCodeChallenge.Models
+{
+    public class Starship
+    {
+        public string Name { get; set; }
+        public string Model { get; set; }
+        public string Starship_Class { get; set; }
+        public string Manufacturer { get; set; }
+        public string Cost_In_Credits { get; set; }
+        public string Length { get; set; }
+        public string Crew { get; set; }
+        public string Passengers { get; set; }
+        public string Max_Atmosphering_Speed { get; set; }
+        public string Hyperdrive_Rating { get; set; }
+        public string MGLT { get; set; }
+        public string Cargo_Capacity { get; set; }
+        public string Consumables { get; set; }
+        public string Url { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
---
+++ MobileCodeChallenge/MobileCodeChallenge/Models/StarshipsResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MobileCodeChallenge.Models
+{
+    class StarshipsResponse
+    {
+        public int Count { get; set; }
+        public string Next { get; set; }
+        public object Previous { get; set; }
+        public List<Starship> Results { get; set; }
+    }
+}
